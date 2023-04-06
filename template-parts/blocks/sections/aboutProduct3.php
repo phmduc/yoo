ba806578fb19fe5ec20164d3bc980c7ed55fe764
ngoisao962 <luongvanchuong963@gmail.com>
2023-04-06 15:24:41 +0700

    steps&product3

--- template-parts/blocks/sections/aboutProduct3.php
+++ template-parts/blocks/sections/aboutProduct3.php
@@ -31,7 +31,74 @@ if( !empty($block['align']) ) {
 // Load values and assign defaults.
 ?>
 <section id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className); ?>">
-    <div class="text-center">About Product 3</div>
+    <div class="container">
+        <div class="aboutproduct_3 text-center">
+            <div class="col-12">
+                
+                <div class="aboutproduct3_box_centent">
+                    <div class="product3_box">
+                        <div class="content_product3">
+                            <img src="<?=  get_stylesheet_directory_uri() . '/assets/images/yoosun_rauma.png' ?>" alt="">
+                            <h4 class="product3_title">Cực kỳ lành tính</h4>
+                        </div> 
+                        <div class="product3_item box_one">
+                            <div class="product_img">
+                            <img src="<?=  get_stylesheet_directory_uri() . '/assets/images/image_36.png' ?>" alt="">
+                            </div>
+                            <div class="product_item_center">
+                            <h6 class="product_item_h6">
+                                D-Panthenol
+                            </h6>
+                            <p class="product_item_p">
+                                Làm dịu da,giảm ngứa rát cho da,làm trơn , mềm da
+                            </p>
+                            </div>
+                        </div>  
+                        <div class="product3_item box_two">
+                            <div class="product_img">
+                            <img src="<?=  get_stylesheet_directory_uri() . '/assets/images/image_38.png' ?>" alt="">
+                            </div>
+                            <div class="product_item_center">
+                            <h6 class="product_item_h6">
+                                D-Panthenol
+                            </h6>
+                            <p class="product_item_p">
+                            Kích thích tái tạo tế bào da, cải thiện làn da bị tổn thương. Ngoài ra, dân gian còn dùng Rau má để làm mát, ngăn ngừa rôm sảy và mẩn ngứa.
+                            </p>
+                            </div>
+                        </div>  
+                        <div class="product3_item box_three">
+                            <div class="product_img">
+                            <img src="<?=  get_stylesheet_directory_uri() . '/assets/images/image_38.png' ?>" alt="">
+                            </div>
+                            <div class="product_item_center">
+                            <h6 class="product_item_h6">
+                            CHLORHEXIDINE DIGLUCONATE 
+                            </h6>
+                            <p class="product_item_p">
+                            Bảo vệ da khỏi tác động của vi khuẩn, ngăn ngừa mụn một một cách hiệu quả.
+                            </p>
+                            </div>
+                        </div>  
+                        <div class="product3_item box_four">
+                            <div class="product_img">
+                            <img src="<?=  get_stylesheet_directory_uri() . '/assets/images/image_37.png' ?>" alt="">
+                            </div>
+                            <div class="product_item_center">
+                            <h6 class="product_item_h6">
+                            VITAMIN E
+                            </h6>
+                            <p class="product_item_p">
+                            Ngăn ngừa sự lão hóa do tác dụng của tia UV và oxy hóa, đồng thời giữ ẩm cho da, giúp da luôn mịn màng.
+                            </p>
+                            </div>
+                        </div>  
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
 <?php 
 add_action( 'wp_footer', 'ap3script', 99, 1 );
