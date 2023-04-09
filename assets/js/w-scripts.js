90a8fdb2af4717cd7edfd7d2fd24c63007aecf05
ngoisao962 <luongvanchuong963@gmail.com>
2023-04-09 22:41:33 +0700

    blogdetail

--- assets/js/w-scripts.js
+++ assets/js/w-scripts.js
@@ -33,6 +33,64 @@ appHeight();
     $(".m-menu").toggleClass("show");
   });
 
+  $(document).ready(function () {
+    $(".blog-slider-list-container").slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      infinite: true,
+      
+      draggable: false,
+      prevArrow: `<button type='button' class='slick-prev'><i class="bi bi-arrow-left"></i></button>`,
+      nextArrow: `<button type='button' class='slick-prev'><i class="bi bi-arrow-right"></i></button>`,
+      dots: true,
+      responsive: [
+        {
+          breakpoint: 1025,
+          settings: {
+            slidesToShow: 2,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            arrows: false,
+            infinite: false,
+          },
+        },
+      ],
+     
+    });
+  });
+  // $( document ).ready(function() {
+  
+
+  //   var $sticky = $('.sticky');
+  //   var $stickyrStopper = $('.sticky-stopper');
+  //   if (!!$sticky.offset()) { // make sure ".sticky" element exists
+  
+  //     var generalSidebarHeight = $sticky.innerHeight();
+  //     var stickyTop = $sticky.offset().top;
+  //     var stickOffset = 0;
+  //     var stickyStopperPosition = $stickyrStopper.offset().top;
+  //     var stopPoint = stickyStopperPosition - generalSidebarHeight - stickOffset;
+  //     var diff = stopPoint + stickOffset;
+  //     console.log(diff)
+  //     $(window).scroll(function(){ // scroll event
+  //       var windowTop = $(window).scrollTop(); // returns number
+  
+  //       if (stopPoint < windowTop) {
+  //           $sticky.css({ position: 'absolute', botttom: 0 });
+  //       } else if (stickyTop < windowTop+stickOffset) {
+  //           $sticky.css({ position: 'fixed', top: 30 });
+  //       } else {
+  //           $sticky.css({position: 'absolute', top: 'initial'});
+  //       }
+  //     });
+  
+  //   }
+  // });
+  
   
 }( jQuery ) );
 
