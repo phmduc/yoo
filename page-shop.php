29c4269258ecd7655df1df9cac65f5080c25e0cb
phmduc <phmduc1012@gmail.com>
2023-04-11 16:23:04 +0700

    push

--- page-shop.php
+++ page-shop.php
@@ -166,71 +166,73 @@ get_header();
         </div>
     </main>
     <div class="w-os pshop-os">
-        <div class="online-shop-container">
-            <div class="os-left">
-                <div class="slider-heading-wrapper">
-                    <h2>Mua hàng online dễ dàng.</h2>
-                    <div class="line"></div>
-                </div>
-                <ul class="os-left-desc">
-                    <li><a href="#" class="os-left-link">Đặc biệt tiện lợi khi chỉ cần vài Click chuột để mau sản
-                            phẩm.</a></li>
-                    <li><a href="#" class="os-left-link">Các sự kiện ưu đãi và trợ giá được thực hiện liên tục.</a>
-                    </li>
-                    <li><a href="#" class="os-left-link">Giao hàng tận nhà nhanh chóng.</a></li>
-                </ul>
-            </div>
-            <div class="os-right">
-                <div class="os-right-bg">
-                    <img src="<?= get_stylesheet_directory_uri() . '/assets/images/cart-os.png' ?>" alt="">
-                    <div class="os-right-click">
-                        <span class="icons-wrapper">
-                            <i class="bi bi-arrow-up-short"></i>
-                        </span>
-                        <span>Click vào liên kết để mua hàng</span>
+        <div class="container">
+            <div class="online-shop-container">
+                <div class="os-left">
+                    <div class="slider-heading-wrapper">
+                        <h2>Mua hàng online dễ dàng.</h2>
+                        <div class="line"></div>
                     </div>
-                    <div class=" os-right-wrapper">
-
-                        <a href="#" class="os-right-link">
-                            <div class="os-right-image">
-                                <img src="<?= get_stylesheet_directory_uri() . '/assets/images/shoppe-os.png' ?>"
-                                    alt="">
-                            </div>
-
-                            <p>Shopee
-                                ĐẠI BẮC
-                                Miền Bắc</p>
-                        </a>
-                        <a href="#" class="os-right-link">
-                            <div class="os-right-image">
-                                <img src="<?= get_stylesheet_directory_uri() . '/assets/images/shoppe-os.png' ?>"
-                                    alt="">
-                            </div>
-
-                            <p>Shopee
-                                ĐẠI BẮC
-                                Miền Nam</p>
-                        </a>
-                        <a href="#" class="os-right-link">
-                            <div class="os-right-image">
-                                <img src="<?= get_stylesheet_directory_uri() . '/assets/images/tiki-os.png' ?>" alt="">
-                            </div>
-                            <p>TIKI
-                                ĐẠI BẮC</p>
-                        </a>
-                        <a href="#" class="os-right-link">
-                            <div class="os-right-image">
-                                <img src="<?= get_stylesheet_directory_uri() . '/assets/images/lazada-os.png' ?>"
-                                    alt="">
-                            </div>
-
-                            <p>LAZADA
-                                ĐẠI BẮC</p>
-                        </a>
-
-                    </div>
+                    <ul class="os-left-desc">
+                        <li><a href="#" class="os-left-link">Đặc biệt tiện lợi khi chỉ cần vài Click chuột để mau sản
+                                phẩm.</a></li>
+                        <li><a href="#" class="os-left-link">Các sự kiện ưu đãi và trợ giá được thực hiện liên tục.</a>
+                        </li>
+                        <li><a href="#" class="os-left-link">Giao hàng tận nhà nhanh chóng.</a></li>
+                    </ul>
                 </div>
-
+                <div class="os-right">
+                    <div class="os-right-bg">
+                        <img src="<?= get_stylesheet_directory_uri() . '/assets/images/cart-os.png' ?>" alt="">
+                        <div class="os-right-click">
+                            <span class="icons-wrapper">
+                                <i class="bi bi-arrow-up-short"></i>
+                            </span>
+                            <span>Click vào liên kết để mua hàng</span>
+                        </div>
+                        <div class=" os-right-wrapper">
+    
+                            <a href="#" class="os-right-link">
+                                <div class="os-right-image">
+                                    <img src="<?= get_stylesheet_directory_uri() . '/assets/images/shoppe-os.png' ?>"
+                                        alt="">
+                                </div>
+    
+                                <p>Shopee
+                                    ĐẠI BẮC
+                                    Miền Bắc</p>
+                            </a>
+                            <a href="#" class="os-right-link">
+                                <div class="os-right-image">
+                                    <img src="<?= get_stylesheet_directory_uri() . '/assets/images/shoppe-os.png' ?>"
+                                        alt="">
+                                </div>
+    
+                                <p>Shopee
+                                    ĐẠI BẮC
+                                    Miền Nam</p>
+                            </a>
+                            <a href="#" class="os-right-link">
+                                <div class="os-right-image">
+                                    <img src="<?= get_stylesheet_directory_uri() . '/assets/images/tiki-os.png' ?>" alt="">
+                                </div>
+                                <p>TIKI
+                                    ĐẠI BẮC</p>
+                            </a>
+                            <a href="#" class="os-right-link">
+                                <div class="os-right-image">
+                                    <img src="<?= get_stylesheet_directory_uri() . '/assets/images/lazada-os.png' ?>"
+                                        alt="">
+                                </div>
+    
+                                <p>LAZADA
+                                    ĐẠI BẮC</p>
+                            </a>
+    
+                        </div>
+                    </div>
+    
+                </div>
             </div>
         </div>
     </div>
